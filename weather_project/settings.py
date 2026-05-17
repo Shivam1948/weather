f0fe94c8057518a307d32f8f763833b2e730a427
Shivam Singh <shivamsingh85732004@gmail.com>
2026-05-17 20:39:29 +0530

    Serve static files from source on Render

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -157,3 +157,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
