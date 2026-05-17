9d602a7e9ea77451c70e4a9a0226b9dd31eaa639
Shivam Singh <shivamsingh85732004@gmail.com>
2026-05-17 20:40:56 +0530

    Add fallback static route for Render

--- weather_project/urls.py
+++ weather_project/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.conf import settings
+from django.views.static import serve
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path('', include('forecast.urls')),
     path('admin/', admin.site.urls),
+    re_path(
+        r'^static/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.BASE_DIR / 'static'},
+    ),
 ]
